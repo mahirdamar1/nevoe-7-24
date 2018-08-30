c2c5c3a9cf0ca59e8921a8e0fbf0bded06e206ec
mahirdamar1 <42822969+mahirdamar1@users.noreply.github.com>
2018-08-30 16:24:07 +0300

    Delete hafıza.js

--- komutlar/hafıza.js
+++
@@ -1,5 +0,0 @@
-const Discord = require("discord.js");
-
-exports.run = (client, message, args) => {
-  message.reply((process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)+" MB **Kullanılıyor...**");
-}
