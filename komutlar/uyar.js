99d1fe352d79b87e01a3eb3bd48b11a3f57a9359
mahirdamar1 <42822969+mahirdamar1@users.noreply.github.com>
2018-08-30 16:40:04 +0300

    Update uyar.js

--- komutlar/uyar.js
+++ komutlar/uyar.js
@@ -11,8 +11,8 @@ exports.run = (client, message, args) => {
   let guild = message.guild
   let reason = args.slice(1).join(' ');
   let user = message.mentions.users.first();
-  let modlog = guild.channels.find('name', 'mod-log');
-  if (!modlog) return message.reply('`mod-log` kanalını bulamıyorum.');
+  let modlog = guild.channels.find('name', 'ceza-takip-listesi');
+  if (!modlog) return message.reply('`ceza-takip-listesi` kanalını bulamıyorum.');
   if (reason.length < 1) return message.reply('Uyarı sebebini yazmalısın.');
   if (message.mentions.users.size < 1) return message.reply('Kimi uyaracağını yazmalısın.').catch(console.error);
   const embed = new Discord.RichEmbed()
