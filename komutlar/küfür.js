13f2088534ad198248d5c671690085c0ba327d0f
mahirdamar1 <42822969+mahirdamar1@users.noreply.github.com>
2018-08-31 05:21:49 +0300

    Add files via upload

---
+++ komutlar/küfür.js
@@ -0,0 +1,6 @@
+﻿const kufurlu = ["macun", "yaprak", "pisi", "gözlük"];
+// Eğer mesasj yukarıdaki kelimeleri içeriyorsa:
+if( kufurlu.some(word =&gt; message.content.includes(word)) ) {
+// Mesajı sil:
+message.delete()
+}
