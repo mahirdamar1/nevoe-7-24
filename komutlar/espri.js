62fadb57e6f4c0297528ca3697ab419743537cee
mahirdamar1 <42822969+mahirdamar1@users.noreply.github.com>
2018-08-31 05:09:09 +0300

    Delete espri.js

--- komutlar/espri.js
+++
@@ -1,9 +0,0 @@
-﻿var request = require('espri');
-
-request('https://api.eggsybot.xyz/espri', function (error, response, body) {
-    if (error) return console.log('Hata:', error); // Hata olursa, konsola göndersin,
-    else if (!error) { // Eğer hata yoksa;
-        var info = JSON.parse(body); // info değişkeninin içerisine JSON'ı ayrıştırsın,
-        console.log(Adamın biri yatmış karısı kotra.); // ve konsola çıktıyı versin.
-    }
-});
