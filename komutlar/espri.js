85019e472e9183afda4b305045c14cf25015196e
mahirdamar1 <42822969+mahirdamar1@users.noreply.github.com>
2018-08-31 05:08:15 +0300

    Update espri.js

--- komutlar/espri.js
+++ komutlar/espri.js
@@ -4,6 +4,6 @@ request('https://api.eggsybot.xyz/espri', function (error, response, body) {
     if (error) return console.log('Hata:', error); // Hata olursa, konsola göndersin,
     else if (!error) { // Eğer hata yoksa;
         var info = JSON.parse(body); // info değişkeninin içerisine JSON'ı ayrıştırsın,
-        console.log(info.soz); // ve konsola çıktıyı versin.
+        console.log(Adamın biri yatmış karısı kotra.); // ve konsola çıktıyı versin.
     }
 });
