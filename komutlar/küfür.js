8524e090248c9f1fb172d4ebbd789da5c503a3e9
mahirdamar1 <42822969+mahirdamar1@users.noreply.github.com>
2018-08-31 05:23:31 +0300

    Delete küfür.js

--- komutlar/küfür.js
+++
@@ -1,6 +0,0 @@
-﻿const kufurlu = ["macun", "yaprak", "pisi", "gözlük"];
-// Eğer mesasj yukarıdaki kelimeleri içeriyorsa:
-if( kufurlu.some(word =&gt; message.content.includes(word)) ) {
-// Mesajı sil:
-message.delete()
-}
